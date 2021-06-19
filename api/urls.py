de25f02afd4b7ed42548e1266efa98df9fa892d7
Вадим <vadim624790@mail.ru>
2021-06-19 18:47:27 +0500

    isort + flake8

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
+
 from . import views
 
-
 urlpatterns = [
-    path("ingredients/", views.Ingredients.as_view()),
-    path("subscriptions/", views.Subscribes.as_view()),
-    path("subscriptions/<int:author_id>/", views.Subscribes.as_view()),
-    path("purchases/", views.Purchases.as_view()),
-    path("purchases/<int:recipe_id>/", views.Purchases.as_view()),
-    path("favorites/", views.Favorites.as_view()),
-    path("favorites/<int:recipe_id>/", views.Favorites.as_view()),
+    path('ingredients/', views.Ingredients.as_view()),
+    path('subscriptions/', views.Subscribes.as_view()),
+    path('subscriptions/<int:author_id>/', views.Subscribes.as_view()),
+    path('purchases/', views.Purchases.as_view()),
+    path('purchases/<int:recipe_id>/', views.Purchases.as_view()),
+    path('favorites/', views.Favorites.as_view()),
+    path('favorites/<int:recipe_id>/', views.Favorites.as_view()),
 ]
