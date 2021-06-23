b3c1baa302818f421a4ee8dfa57f1cd562d150fe
Вадим <vadim624790@mail.ru>
2021-06-24 02:47:08 +0500

    fix settings

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -119,8 +119,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER') or \
-    'yapraktikumsky@gmail.com'
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_USER') or '624790vadiM'
+EMAIL_HOST_USER = 'yapraktikumsky@gmail.com'
+EMAIL_HOST_PASSWORD = '624790vadiM'
 
 POSTS__PER_PAGE = 6
