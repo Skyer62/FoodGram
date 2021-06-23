a47bda4d0e898b9ea93f6b7980894bd98da8173a
Вадим <vadim624790@mail.ru>
2021-06-23 16:56:32 +0500

    prepare for deploy

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -6,7 +6,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = os.environ.get('SECRET_KEY_SETTINGS')
+SECRET_KEY = os.environ.get('SECRET_KEY_SETTINGS') or 'example'
 
 DEBUG = False
 
